39255883917ab20bcc232b41c8e6913029aa01c2
smutek <ssmutekk@proton.me>
2025-04-17 17:05:29 -0400

    prob works
    idk tho :3

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 echo 'installing funny :3'
 
-sudo pacman -S waybar ghostty tar unzip upower hyprland-meta flatpak songrec --noconfirm
+sudo pacman -S waybar ghostty tar unzip upower hyprland-meta flatpak songrec tofi --noconfirm
 
 cd ~/Downloads
 
-mkdir -p ~/.config/waybar
-mkdir -p ~/.config/hypr
-
 tar -xzvf dotFiles.tar.gz
+
+mkdir -p ~/.config/hypr
 
 cd dotFiles
 
-mv config.jsonc ~/.config/waybar
-mv style.css ~/.config/waybar
+mv waybar ~/.config/waybar
+
+mv ghostty ~/.config/ghostty
 
 mv binds.conf ~/.config/hypr
 mv rules.conf ~/.config/hypr
@@ -23,17 +23,7 @@ mv input.conf ~/.config/hypr
 mv colours.conf ~/.config/hypr
 mv env.conf ~/.config/hypr
 
-mkdir -p ~/.config/ghostty
-
-mv ghostty.tar.gz ~/.config/ghostty
-tar -xzvf ghostty.tar.gz
-
-rm -rf ghostty.tar.gz
-
-mv bashScripts.tar.gz ~/bashScripts
-tar -xzvf bashScripts.tar.gz
-
-rm -rf bashScripts.tar.gz
+mv bashScripts ~/
 
 cd ~
 
