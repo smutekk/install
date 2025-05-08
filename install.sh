5f84ac2e475d2404465fda5c47058ec460dd5789
smutek <ssmutekk@proton.me>
2025-05-08 14:21:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 echo 'installing funny :3'
 
-sudo pacman -S waybar ghostty tar unzip upower hyprland-meta flatpak songrec tofi --noconfirm
+sudo pacman -S waybar ghostty tar unzip hyprland-meta-git flatpak songrec tofi --noconfirm
 
 tar -xzvf dotFiles.tar.gz
 
-mkdir -p ~/.config/hypr
 mkdir -p ~/Pictures/Wallpapers
-mkdir -p ~/.config/tofi
 
 cd dotFiles
 
-mv waybar ~/.config/waybar
-mv ghostty ~/.config/ghostty
-mv tofi ~/.config/tofi
+sudo rm -rf ~/.config/waybar
+mv waybar ~/.config/
+
+sudo rm -rf ~/.config/ghostty
+mv ghostty ~/.config/
+
+sudo rm -rf ~/.config/tofi
+mv tofi ~/.config/
 
 sudo rm -rf ~/.config/hypr
 mv hypr ~/.config/
@@ -24,7 +27,17 @@ mv bashScripts ~/
 mv gtk-3.0 ~/.config/
 mv gtk-4.0 ~/.config/
 
+mv piratedGames ~/
+mv Old.tar.gz ~/
+mv walltaker ~/Downloads/
+
 cd ~
+
+git clone https://aur.archlinux.org/yay.git
+cd ~/yay
+sudo makepkg -si
+cd ~
+sudo rm -rf ~/yay
 
 flatpak install org.vinegarhq.Vinegar
 flatpak install org.vinegarhq.Sober
